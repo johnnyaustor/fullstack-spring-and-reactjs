939569d33a1245ca70379629b40214a221bfd25c
johnny.austor@gmail.com <johhny.austor@gmail.com>
2019-09-11 17:17:19 +0700

    create project

--- 01/ppmtool-react-client/src/components/Project/AddProject.js
+++ 01/ppmtool-react-client/src/components/Project/AddProject.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { createProject } from "../../actions/projectAction";
 
 class AddProject extends Component {
   constructor() {
@@ -31,7 +34,7 @@ class AddProject extends Component {
       end_date: this.state.end_date
     };
 
-    console.log(newProject);
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
@@ -109,4 +112,11 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+  createProject: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { createProject }
+)(AddProject);
