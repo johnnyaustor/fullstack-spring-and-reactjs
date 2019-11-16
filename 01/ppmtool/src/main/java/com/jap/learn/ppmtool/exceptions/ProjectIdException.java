79c0ee04fed40672324c0c19fbb6e5dee776e75c
triantoro <triantoro@code.id>
2019-11-16 19:59:51 +0700

    project exception

--- 01/ppmtool/src/main/java/com/jap/learn/ppmtool/exceptions/ProjectIdException.java
+++ 01/ppmtool/src/main/java/com/jap/learn/ppmtool/exceptions/ProjectIdException.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ProjectIdException extends RuntimeException {
-
     public ProjectIdException(String message) {
         super(message);
     }
